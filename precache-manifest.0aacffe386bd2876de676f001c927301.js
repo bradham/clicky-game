ead6aff8d97cb59567a135b7e015e736866c5bff
bradham <bradham@users.noreply.github.com>
2019-12-30 01:02:04 -0600

    Updates

---
+++ precache-manifest.0aacffe386bd2876de676f001c927301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5e306206f4f7a302f366a9685dc852",
+    "url": "/clicky-game/public/index.html"
+  },
+  {
+    "revision": "1d94eb144a1adb0d3617",
+    "url": "/clicky-game/public/static/css/main.f9eeb882.chunk.css"
+  },
+  {
+    "revision": "e72562dfe9e9a5e0b393",
+    "url": "/clicky-game/public/static/js/2.e26d3aa1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/public/static/js/2.e26d3aa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d94eb144a1adb0d3617",
+    "url": "/clicky-game/public/static/js/main.14f6b25d.chunk.js"
+  },
+  {
+    "revision": "b2decb75f0a10091b909",
+    "url": "/clicky-game/public/static/js/runtime-main.9c2044cf.js"
+  }
+]);
